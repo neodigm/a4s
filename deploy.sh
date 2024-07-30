655d566f9b3a51dd9b4fab3426c116e35f60fec2
neodigm <3151842+neodigm@users.noreply.github.com>
2024-07-29 19:22:39 -0500

    a4s_dist

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,3 @@ git pull
 git add -A
 git commit -m a4s_dist
 git push
-
-
-**npm run build
-**cd ../interstellar-website
-**git status
-**git pull
-**cd ../interstellar-website-hugo
-**cp -r static/* public/
-**cp -r public/* ../interstellar-website
-**cd ../interstellar-website
-**git status
-**git add -A
-**git commit -m hugo_publish
-**git push
